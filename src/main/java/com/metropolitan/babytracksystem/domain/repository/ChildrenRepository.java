61f7d5218793e06e55b3656a2b13483c69ea84d3
elijah.ndungu <elijahndungu30@gmail.com>
2023-04-17 21:22:00 +0300

    chore: adding method implemantations, changed child repository to a paging repository

--- src/main/java/com/metropolitan/babytracksystem/domain/repository/ChildrenRepository.java
+++ src/main/java/com/metropolitan/babytracksystem/domain/repository/ChildrenRepository.java
@@ -1,9 +1,9 @@
 package com.metropolitan.babytracksystem.domain.repository;
 
 import com.metropolitan.babytracksystem.domain.entity.Children;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChildrenRepository extends JpaRepository<Long, Children> {
+public interface ChildrenRepository extends PagingAndSortingRepository<Long, Children> {
 }
--- src/main/java/com/metropolitan/babytracksystem/services/impl/ChildrenServiceImpl.java
+++ src/main/java/com/metropolitan/babytracksystem/services/impl/ChildrenServiceImpl.java
@@ -1,10 +1,34 @@
 package com.metropolitan.babytracksystem.services.impl;
 
+import com.metropolitan.babytracksystem.domain.dto.ChildrenDto;
+import com.metropolitan.babytracksystem.domain.entity.Children;
 import com.metropolitan.babytracksystem.services.ChildrenService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ChildrenServiceImpl  implements ChildrenService  {
+
+    @Override
+    public Children addChildren(ChildrenDto childrenDto) {
+        return null;
+    }
+
+    @Override
+    public Children updateChildren(Long id, ChildrenDto childrenDto) {
+        return null;
+    }
+
+    @Override
+    public Page<Children> getAllChildren(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Children searchChildrenByName(String name) {
+        return null;
+    }
 }
