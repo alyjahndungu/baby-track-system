cba738da8be4937ce746e004a61179f7b5f3f847
elijah.ndungu <elijahndungu30@gmail.com>
2023-04-18 00:39:37 +0300

    feat: added get scores for a dart game question endpoint

---
+++ src/main/java/com/metropolitan/babytracksystem/controllers/DartGameController.java
@@ -0,0 +1,35 @@
+package com.metropolitan.babytracksystem.controllers;
+
+import com.metropolitan.babytracksystem.domain.dto.DartGameResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "api/v1/dart/scores")
+public class DartGameController {
+    @GetMapping
+    public ResponseEntity<Object> getDartScores(@RequestParam("xCoordinate") double xCoordinate, @RequestParam("yCoordinate") double yCoordinate) {
+        int points = scoreDart(xCoordinate, yCoordinate);
+        DartGameResponse  dartGameResponse = new DartGameResponse(xCoordinate, yCoordinate, points);
+        return ResponseEntity.ok().body(dartGameResponse);
+    }
+    public static int scoreDart(double x, double y) {
+        double area = Math.sqrt(x * x + y * y);
+        if (area > 10.0) {
+            return 0;
+        } else if (area > 5.0) {
+            return 1;
+        } else if (area > 1.0) {
+            return 5;
+        } else {
+            return 10;
+        }
+    }
+
+
+}
---
+++ src/main/java/com/metropolitan/babytracksystem/domain/dto/DartGameResponse.java
@@ -0,0 +1,4 @@
+package com.metropolitan.babytracksystem.domain.dto;
+
+public record DartGameResponse(double xCoordinate, double yCoordinate,  int points) {
+}
