70536c30f41e9b488a252b107bae690c0f3e5bc9
elijah.ndungu <elijahndungu30@gmail.com>
2023-04-17 17:29:18 +0300

    initial project setup and packaging

---
+++ src/test/java/com/metropolitan/babytracksystem/BabyTrackSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metropolitan.babytracksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyTrackSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
